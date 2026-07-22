8827a39aa5aae23b3fe756404f0a27ad7cb853e0
Eder Kenzo Makino Perez <makinoperezederkenzo@gmail.com>
2026-07-22 16:05:46 -0500

    Modificacion de Sidebar

--- frontend/mi-vite/src/components/Sidebar.jsx
+++ frontend/mi-vite/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react" ;
 import "./Sidebar.css";
 import {NavLink} from "react-router-dom";
 
@@ -7,76 +8,70 @@ import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
 import PersonIcon from "@mui/icons-material/Person";
 import LogoutIcon from "@mui/icons-material/Logout";
+import ArrowForwardRoundedIcon from '@mui/icons-material/ArrowForwardRounded';
 
 function Sidebar(){
 
+    const [isClosed, setIsClosed] = useState(true);
+
+    const handleToggle = () => {setIsClosed(!isClosed);
+    };
     return(
 
-        <aside className="sidebar">
-
+        <aside className={`sidebar ${isClosed ? "close" : "" } `}>
             <div className="sidebar-logo">
-                <h2>YUPI</h2>
-                <span>Playground</span>
+                <span className="text">YUPI</span>
+                <span className="text">Playground</span>
+                <button className="toggle" onClick={handleToggle}>
+                    <ArrowForwardRoundedIcon/>
+                </button>
             </div>
 
             <nav>
                 <ul>
-
                     <li>
                     <NavLink to="/menu" className="menu-link">
                         <DashboardIcon/>
-                        <span>Dashboard</span>
+                        <span className="text">Dashboard</span>
                     </NavLink>    
                     </li>
 
                     <li>
                         <NavLink to="/clientes" className="menu-link">
                             <GroupsIcon />
-                            <span>Clientes y Niños</span>
+                            <span className="text">Clientes y Niños</span>
                         </NavLink>
                     </li>
 
                     <li>
                         <NavLink to="/reservas" className="menu-link">
                             <CalendarMonthIcon />
-                            <span>Reservas</span>
+                            <span className="text">Reservas</span>
                         </NavLink>
                     </li>
-
                     <li>
                         <NavLink to="/caja" className="menu-link">
                             <PointOfSaleIcon />
-                            <span>Caja</span>
+                            <span className="text">Caja</span>
                         </NavLink>
                     </li>
-
                     <li>
                         <NavLink to="/usuarios" className="menu-link">
                             <PersonIcon />
-                            <span>Usuarios</span>
+                            <span className="text">Usuarios</span>
                         </NavLink>
                     </li>
-
                 </ul>
             </nav>
-        
+
             <div className="sidebar-footer">
-
                 <button className="logout-btn">
-
                     <LogoutIcon />
-
-                    <span>Cerrar sesión</span>
-
+                    <span className="text">Cerrar sesión</span>
                 </button>
-
             </div>
-
         </aside>
-
-
     );
-
 }
 
 export default Sidebar;
